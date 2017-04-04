5f3d653de65912506b1e35a4b34d11d459c4928b
Alex Pate <alex@alexpate.uk>
2017-04-04 21:28:33 +0100

    👀

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const cssStandards = require('spike-css-standards')
-const latest = require('babel-preset-latest')
-const reshape = require('reshape')
-const include = require('reshape-include')
-const layouts = require('reshape-layouts')
-const content = require('reshape-content')
-const expressions = require('reshape-expressions')
+const cssStandards = require('spike-css-standards');
+const latest = require('babel-preset-latest');
+const reshape = require('reshape');
+const include = require('reshape-include');
+const layouts = require('reshape-layouts');
+const content = require('reshape-content');
+const expressions = require('reshape-expressions');
 
-const listings = require('./data/listings.json')
+const listings = require('./data/listings.json');
 
 module.exports = {
   devtool: 'source-map',
@@ -27,4 +27,4 @@ module.exports = {
     return cssStandards({ parser: false, webpack: ctx })
   },
   babel: { presets: [latest] }
-}
+};
